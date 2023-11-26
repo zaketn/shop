47984e68a023a3b88dc6db47200643bf4f421790
zaketn <specot70@gmail.com>
2023-11-27 01:41:46 +0500

    refactor: Добавлена обработка ошибок при проблемах с доступом к telegram

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -2,19 +2,30 @@
 
 namespace App\Services\Telegram;
 
-use Illuminate\Http\Client\Response;
+use App\Services\Telegram\Exceptions\TelegramBotApiException;
 use Illuminate\Support\Facades\Http;
+use Throwable;
 
 class TelegramBotApi
 {
     private const URL = 'https://api.telegram.org/bot';
 
-    public static function sendMessage(string $botToken, string $chatId, string $message): Response
+    public static function sendMessage(string $botToken, string $chatId, string $message): bool
     {
-        return Http::get(self::URL . "$botToken/sendMessage", [
-            'chat_id' => $chatId,
-            'text' => $message,
-            'parse_mode' => 'html'
-        ]);
+        try{
+            $response = Http::get(self::URL . "$botToken/sendMessage", [
+                'chat_id' => $chatId,
+                'text' => $message,
+                'parse_mode' => 'html'
+            ])
+                ->throw()
+                ->json();
+
+            return $response['ok'] ?? false;
+        } catch (Throwable $e) {
+            report(new TelegramBotApiException($e->getMessage()));
+
+            return false;
+        }
     }
 }
